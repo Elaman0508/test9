b97ce7a3be1259b9c5e3c76a494a5f5dcbb7e6b9
sulaevatumar67-sys <sulaevatumar67@gmail.com>
2025-12-01 16:35:40 +0600

    chess

---
+++ chess.py
@@ -0,0 +1,14 @@
+pos_x = int(input("x: "))
+pos_y = int(input("y: "))
+for i in range(1,9):
+    for j in range(1,9):
+        if i == pos_y and j == pos_x:                    
+            print("F", end=' ')
+        elif i == pos_y or j == pos_x:                   
+            print("1", end=' ')
+        elif abs( i - pos_y) == abs( j - pos_x):         
+            print("1", end=' ')
+        else:
+            print("0", end=' ')
+    print()
+    
